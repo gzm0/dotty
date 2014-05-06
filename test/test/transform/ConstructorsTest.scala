f6e707376df0791a1b8d2b06bd90fdac60380801
Tobias Schlatter <tobias@meisch.ch>
2014-05-06 09:15:05 +0200

    even simpler test

--- test/test/transform/ConstructorsTest.scala
+++ test/test/transform/ConstructorsTest.scala
@@ -7,13 +7,13 @@ import org.junit.Assert
 class ConstructorsTest extends DottyTest {
 
   @Test
-  def printTest = checkCompile("constructors",
+  def printTest = checkCompile("intercepted",
       """
-      class A(x: Int) {
+      class A/*(x: Int) {
 
-        def this() = this(4)
+        //def this() = this(4)
 
-      }
+      }*/
       """
   ) { (tree, context) =>
 
